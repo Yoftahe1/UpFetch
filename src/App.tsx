73af93cd80f8a41952294702b83cc6622ad9b0d4
Yoftahe Merkebu <yoftahemerkebu2@gmail.com>
2024-08-20 12:57:06 +0300

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,70 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
+
 import { io } from "socket.io-client";
+
+import icon from "../public/up.png";
 
 import "./App.css";
 
 const socket = io("http://localhost:3333");
 
 function App() {
-  const [data, setData] = useState<string[]>([]);
-  // useEffect(() => {
-  //   chrome.runtime.onMessage.addListener((request, _, sendResponse) => {
-  //     if (request.action === "update_popup") {
-  //       // Update the popup UI with the received data
-  //       const data = request.data;
-  //       setData(data);
-  //       sendResponse({ status: "Message received in background" });
-  //     }
-  //   });
-  // }, []);
+  const [isSubscribed, setIsSubscribed] = useState(false);
+  const inputRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
-    // Listen for incoming messages
-    socket.on('jobs', (msg) => {
-      console.log(msg)
+    socket.on("jobs", (jobs: [{ title: any; link: any; description: any }]) => {
+      jobs.map((job) => {
+        chrome.notifications.create(job.link, {
+          type: "basic",
+          iconUrl: icon,
+          title: job.title,
+          message: job.description,
+          priority: 2, 
+        });
+      });
     });
 
     return () => {
-      socket.off('jobs');
+      socket.off("jobs");
     };
   }, []);
 
-  function handleClick(){
-    const URL=Math.floor(Math.random()*100)
-    socket.emit('subscribe', URL);
+  useEffect(() => {
+    chrome.storage.local.get(["URL"], function (result) {
+      if (result.URL != undefined) {
+        inputRef.current!.value = result.URL;
+      }
+    });
+  }, []);
+
+  function handleSubscribe() {
+    const URL = inputRef.current?.value;
+    chrome.storage.local.set({ URL });
+    socket.emit("subscribe", URL);
+    setIsSubscribed(true);
+  }
+
+  function handleUnsubscribe() {
+    socket.emit("unsubscribe");
+    setIsSubscribed(false);
   }
 
   return (
-    <div className="ext">
-      <h3>UpFetch</h3>
+    <>
       <div>
-        <input />
-        <button onClick={handleClick}>save</button>
+        <h1>Up-Fetch</h1>
+        <input ref={inputRef} placeholder="advanced search url" />
+        {isSubscribed ? (
+          <button className="warn" onClick={handleUnsubscribe}>
+            Unsubscribe
+          </button>
+        ) : (
+          <button onClick={handleSubscribe}>Subscribe</button>
+        )}
       </div>
-      <div>
-        {data.map((v, i) => {
-          return <p key={i}>{v}</p>;
-        })}
-      </div>
-    </div>
+      <p>@ ALL-RIGHTS RESERVED TO UP-FETCH</p>
+    </>
   );
 }
 
